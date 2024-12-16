45e7224e22a76879c7bed04cf11b40b392321a88
waves0217 <waves0217@gmail.com>
2024-12-16 18:10:27 +0900

    feat: add PointController, PointService

---
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -0,0 +1,49 @@
+package io.hhplus.tdd.point;
+
+import io.hhplus.tdd.ErrorResponse;
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PointService {
+
+    private final UserPointTable userPointTable;
+    private final PointHistoryTable pointHistoryTable;
+
+    public UserPoint getId(long id) {
+        return userPointTable.selectById(id);
+    }
+
+    public List<PointHistory> getPointHistory(long id) {
+        return pointHistoryTable.selectAllByUserId(id);
+    }
+
+    public UserPoint chargePoint(long id, long amount, long chargeDate) {
+        UserPoint userPoint = userPointTable.selectById(id);
+
+        UserPoint chargedUserPoint = userPointTable.insertOrUpdate(id, userPoint.point() + amount);
+
+        pointHistoryTable.insert(id,amount,TransactionType.CHARGE,chargeDate);
+
+        return chargedUserPoint;
+    }
+
+    public UserPoint usePoint(long id, long amount, long useDate) {
+        UserPoint userPoint = userPointTable.selectById(id);
+
+        if(userPoint.point() < amount){
+            throw new IllegalArgumentException("포인트가 부족합니다.");
+        }
+
+        UserPoint useUserPoint = userPointTable.insertOrUpdate(id, userPoint.point() - amount);
+        pointHistoryTable.insert(id,amount,TransactionType.USE,useDate);
+        return useUserPoint;
+    }
+
+}
